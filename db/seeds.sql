db3c64f55b11aaa420fb57bcb0e4fad3ebbf1f65
Loren <lorenrose@icloud.com>
2017-11-02 19:55:00 -0500

    database

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plymouth Roquefort Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('I Know Why the Cajun Burger Sings', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Poutine on the Ritz Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom with a View Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sweet Home Avocado Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('A Good Manchego is Hard to Find Burger', true);
